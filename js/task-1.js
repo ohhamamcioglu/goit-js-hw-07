17ad5b7fc62cb08f15b4e357d793215a47a83959
Ömer Hilmi Hamamcıoğlu <ohhamamcioglu@gmail.com>
2025-04-14 20:48:13 +0300

    v-1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+// 1. Tüm .item öğelerini seç
+const categories = document.querySelectorAll('#categories .item');
+
+// 2. Kaç kategori olduğunu konsola yazdır
+console.log(`Number of categories: ${categories.length}`);
+
+// 3. Her kategori için başlık ve içerik sayısını yazdır
+categories.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const itemsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemsCount}`);
+});
---
+++ js/task-4.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  form.reset();
+});
+
